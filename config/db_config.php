84eaa4839b2d538472f890870f508a53a355e967
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2024-02-24 20:23:09 +0100

    Passaggio a CodeIgniter

--- config/db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db_config = [
-    'db_engine' => 'mysql',
-    'db_host' => '127.0.0.1',
-    'db_name' => 'myEcom',
-    'db_user' => 'root',
-    'db_password' => '1234567890',
-];
--- inc/render.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Render {
-    public function view($view_filename, $data = []){
-        $data = (object) $data;
-        include_once("../views/{$view_filename}.php");
-    }
-}
