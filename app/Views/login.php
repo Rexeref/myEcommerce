37b11c0f8496e23b1eb69e479a24e52d3eb0b787
Vincenzo Nikodem Cesqui Podiani <rexeref@gmail.com>
2024-02-26 11:03:33 +0100

    Creazione Viste

---
+++ app/Views/login.php
@@ -0,0 +1,29 @@
+<h2><?= esc($title) ?></h2>
+
+<?php 
+    
+    if (! empty($prodotti) && is_array($prodotti)):
+?>
+
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+<?php else: ?>
+
+    You're already logged in!
+
+<?php endif ?>
